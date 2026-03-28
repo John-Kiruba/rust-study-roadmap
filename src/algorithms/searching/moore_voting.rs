5fc74c17fa3a32db6650afc2d0318e59b6dbc4ff
jkirubai <ijohnkiruba@gmail.com>
2026-03-29 00:04:46 +0530

    Added Moore_Voting Algorithm

---
+++ src/algorithms/searching/moore_voting.rs
@@ -0,0 +1,37 @@
+fn moore_voting(arr: &[i32]) -> i32 {
+    let n = arr.len();
+
+    let mut ele = 0;
+    let mut cnt = 0;
+
+    for &item in arr.iter() {
+        if cnt == 0 {
+            ele = item;
+        } else if item == ele {
+            cnt += 1;
+        } else {
+            cnt -= 1;
+        }
+    }
+
+    let cnt_check = arr.iter().filter(|&&x| x == ele).count();
+
+    if cnt_check > (n / 2) {
+        ele
+    } else {
+        -1
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_moore_voting() {
+        let arr1: Vec<i32> = vec![9, 1, 8, 1, 1];
+        assert!(moore_voting(&arr1) == 1);
+        let arr2: Vec<i32> = vec![1, 2, 3, 4];
+        assert!(moore_voting(&arr2) == -1);
+    }
+}
