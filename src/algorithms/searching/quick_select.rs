c950783a778b23dccfd804f359ce267af64a24a9
jkirubai <ijohnkiruba@gmail.com>
2026-04-01 15:37:12 +0530

    Added Test for quick_select

--- src/algorithms/searching/quick_select.rs
+++ src/algorithms/searching/quick_select.rs
@@ -24,6 +24,21 @@ fn partition(list: &mut [i32], left: usize, right: usize) -> usize {
             p += 1;
         }
     }
-    list.swap(right, p);
+    list.swap(p, right);
     p
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut arr1 = [2, 3, 4, 5];
+        assert_eq!(quick_select(&mut arr1, 0, 3, 1), 3);
+        let mut arr2 = [2, 5, 9, 12, 16];
+        assert_eq!(quick_select(&mut arr2, 1, 3, 2), 9);
+        let mut arr2 = [0, 3, 8];
+        assert_eq!(quick_select(&mut arr2, 0, 0, 0), 0);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,2 @@
-//pub mod algorithms;
 mod algorithms;
-use algorithms::searching::quick_select::quick_select;
-fn main() {
-    let mut data = [2, 5, 1, 67, 1, 3, 2, 6];
-    let kl = 3;
-    let len = data.len();
-    let target_index = len - kl;
-    let result = quick_select(&mut data, 0, len - 1, target_index);
-    println!(
-        "The {}-th largest element in the data: {:?}, is: {}",
-        kl, data, result
-    );
-}
+fn main() {}
